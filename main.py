80f6ba5eb234a9f3643b47664a29fa8673e7aaad
matsuchan <166775967+matsuchan@users.noreply.github.com>
2024-06-21 16:56:58 +0900

    html

--- main.py
+++ main.py
@@ -35,13 +35,19 @@ def omikuji():
 @app.get("/index")
 def index():
     html_content = """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look ma! HTML!</h1>
-        </body>
-    </html>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+</head>
+<body>
+  <h1 style="background-color: #00ff00;">見出しレベル1</h1>
+  <h2 style="color: #ffffff; background-color: #00ff00;">見出しレベル2</h2>
+  <p style="color: #ff0000">あいうえお</p>
+  <h2 style="color: #ffffff; background-color: #00ff00;">見出しレベル2</h2>
+  <p style="color: #ff0000">かきくけこ</p>
+  <a href="https://www.google.co.jp/" title="検索">ググる</a>
+</body>
+</html>
     """
     return HTMLResponse(content=html_content, status_code=200)
